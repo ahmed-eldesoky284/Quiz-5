762c204c64051c70a966b5f20d2b367ec9cc166d
ENG\Ahmed Eldesoky <66114762+ahmed-eldesoky284@users.noreply.github.com>
2023-09-17 00:05:43 +0300

    Update qiuz_51.ino

--- qiuz_51.ino
+++ qiuz_51.ino
@@ -1,16 +1,7 @@
 const byte tilt_pin = 2;
 unsigned long STime = 0;
 unsigned long ETime = 0;
-bool IsPrinting = false;
-bool IsTilted = false;
-
-enum State {
-  WAIT_A_TILT,
-  TILT_DETECTED,
-  WAIT_A_RELEASE
-};
-
-State currentState = WAIT_A_TILT;
+bool print = false;
 
 void setup() {
   Serial.begin(9600);
@@ -19,33 +10,17 @@ void setup() {
 
 void loop() {
   int tiltState = digitalRead(tilt_pin);
-
-  switch (currentState) {
-    case WAIT_A_TILT:
-      if (tiltState == HIGH) {
-        STime = millis();
-        IsTilted = true;
-        currentState = TILT_DETECTED;
-      }
-      break;
-
-    case TILT_DETECTED:
-      if (tiltState == LOW) {
-        ETime = millis();
-        IsTilted = false;
-        unsigned long durationMillis = ETime - STime;
-        float durationSeconds = (float)durationMillis / 1000;
-        Serial.print("Time Duration: ");
-        Serial.print(durationSeconds);
-        Serial.println(" s");
-        currentState = WAIT_A_RELEASE;
-      }
-      break;
-
-    case WAIT_A_RELEASE:
-      if (tiltState == HIGH) {
-        currentState = WAIT_A_TILT;
-      }
-      break;
+   if (tiltState == 1 && !print) {
+    STime = millis();
+    print = true;
+  } else if (tiltState == 0 && print) {
+    ETime = millis();
+    unsigned long Millis = ETime - STime;
+    float Seconds = (float)Millis / 1000;
+    Serial.print("Time: ");
+    Serial.print(Seconds);
+    Serial.println(" s");
+    delay(100); 
+    print = false;
   }
 }
